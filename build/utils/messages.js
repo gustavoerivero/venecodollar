b7f3ac6f46ab04cf16d7e3b1cb3444747d56df27
Gustavo Rivero <gustavoerivero12@gmail.com>
2023-06-10 16:03:04 -0400

    Delete build directory

--- build/utils/messages.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var messages = {
-    "Success": "Success",
-    "Error": "Error",
-    "UnexpectedError": "Unexpected error"
-};
-exports.default = messages;
